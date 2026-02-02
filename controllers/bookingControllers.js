ec9b735b9644311f4f629e960e22daa3e342e044
Khiem Khanh <khiemkhanh.szt@gmail.com>
2026-02-02 17:32:34 +0700

    fix stripe 3

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -59,11 +59,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   let event = req.body;
   const signature = req.headers['stripe-signature'];
 
@@ -79,7 +79,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed') {
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
   }
 
   res.status(200).json({ received: true });
